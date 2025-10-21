1df93670603eb951204b5b9e127a336a01daba03
Sebastian Lempik <sebastian.lempik@gmx.de>
2025-10-21 17:43:01 +0200

    Release version 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "2.0.0"
 }
 
 group = "io.github.selemba1000"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -27,9 +28,14 @@ kotlin {
 
 gradlePlugin{
     plugins{
+        website = "https://github.com/selemba1000/gradle-zig"
+        vcsUrl = "https://github.com/selemba1000/gradle-zig"
         create("gradle-zig"){
             id = "io.github.selemba1000.gradle-zig"
             implementationClass = "io.github.selemba1000.GradleZigPlugin"
+            displayName = "Gradle Zig Plugin"
+            description = "A Gradle plugin for seamless integration of the Zig build system into your Java/Kotlin projects."
+            tags = listOf("zig", "jna", "native")
         }
     }
 }
